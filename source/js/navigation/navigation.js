690992989f63f0dd882b77ff539472b2503f29d0
Ксения <easyghoul@gmail.com>
2023-12-03 10:39:43 +0700

    Модернизирует функционал открытия навигационного меню

--- source/js/navigation/navigation.js
+++ source/js/navigation/navigation.js
@@ -1,20 +1,49 @@
-const list = document.querySelector('.navigation__list');
+const menu = document.querySelector('.navigation__list');
 const button = document.querySelector('.navigation__button');
 const toggle = button.querySelector('.navigation__toggle');
 const clue = button.querySelector('.navigation__clue');
+const TABLET_WIDTH = 640;
 
-let listHeight = list.offsetHeight;
-list.style.transform = `translateY(-${listHeight}px)`;
+const openList = () => {
+  toggle.classList.add('navigation__toggle--open');
+  menu.classList.add('navigation__list--open');
+  clue.textContent = 'Закрыть меню.';
+  menu.style.transform = `translateY(0)`;
+};
 
-button.addEventListener('click', () => {
-  toggle.classList.toggle('navigation__toggle--open');
-  list.classList.toggle('navigation__list--open');
-  listHeight = list.offsetHeight;
-  if (list.classList.contains('navigation__list--open')) {
-    clue.textContent = 'Закрыть меню.';
-    list.style.transform = `translateY(0)`;
-  } else {
-    clue.textContent = 'Открыть меню.';
-    list.style.transform = `translateY(-${listHeight}px)`;
-  }
+const closeList = () => {
+  toggle.classList.remove('navigation__toggle--open');
+  menu.classList.remove('navigation__list--open');
+  clue.textContent = 'Открыть меню.';
+  menu.style.transform = `translateY(-${menu.offsetHeight}px)`;
+};
+
+const onButtonClick = () => {
+  menu.classList.contains('navigation__list--open')
+    ? closeList()
+    : openList();
+};
+
+const renderMenu = () => {
+  menu.classList.contains('navigation__list--open')
+    ? openList()
+    : closeList();
+
+  button.addEventListener('click', onButtonClick);
+};
+
+const closeMenu = () => {
+  menu.style.transform = `translateY(0)`;
+  button.removeEventListener('click', onButtonClick);
+};
+
+if (window.screen.width < TABLET_WIDTH) {
+  menu.style.transform = `translateY(-${menu.offsetHeight}px)`;
+  renderMenu();
+}
+
+window.addEventListener('resize', () => {
+  window.screen.width < TABLET_WIDTH
+    ? renderMenu()
+    : closeMenu();
 });
